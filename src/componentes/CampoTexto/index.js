547c7b3888b4b9a6e15fdc1f5fa045e6757bdb38
Erick Hayakawa <hayakawa0508@gmail.com>
2025-05-15 17:18:55 -0300

    limpando código e fazendo form

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,14 @@
+import './CampoTexto.css'
+
+const CampoTexto = (props) => {
+    return (
+        <div className="CampoTexto">
+            <label>
+                {props.label}
+            </label>
+            <input placeholder={props.placeholder}></input>
+        </div>
+    )
+}
+
+export default CampoTexto
