95f2de4a547b68a12ff90a60d7e4c1ed2ea2ef86
Erick Hayakawa <hayakawa0508@gmail.com>
2025-05-18 15:10:51 -0300

    solucionando erros de import do css

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,5 +1,5 @@
 import { AiFillCloseSquare } from "react-icons/ai";
-import './colaborador.css'
+import './Colaborador.css'
 
 const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
     return (<div className="colaborador">
