9fbd6d51da2b73dc0be705da0fd355f11301e5e5
Erick Hayakawa <hayakawa0508@gmail.com>
2025-05-18 16:30:41 -0300

    adicionando funcionalidade de favoritar o colaborador

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,7 +1,17 @@
 import { AiFillCloseSquare } from "react-icons/ai";
+import { CiHeart } from "react-icons/ci";
+import { FaHeart } from "react-icons/fa";
 import './Colaborador.css'
 
-const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar }) => {
+    function favoritar(){
+        aoFavoritar(colaborador.id)
+    }
+
+    const propsFavorito = {
+        size: 25,
+        onClick: favoritar
+    }
     return (<div className="colaborador">
         <AiFillCloseSquare 
             size={25} 
@@ -14,6 +24,12 @@ const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
         <div className="rodape">
             <h4>{colaborador.nome}</h4>
             <h5>{colaborador.cargo}</h5>
+            <div className="favoritar">
+                {colaborador.favorito 
+                ? <FaHeart {...propsFavorito} color="#ff0000" /> 
+                : <CiHeart {...propsFavorito} />
+            }
+            </div>
         </div>
     </div>)
 }
